e56a5c3bf2e64067119045486e5e120e983126da
fiarr4ik <fiarr4ik@gmail.com>
2025-03-12 11:17:00 +0700

    init commit

---
+++ src/main/java/ru/fiarr4ikdev/componentaccountingapi/ComponentAccountingApiApplication.java
@@ -0,0 +1,14 @@
+package ru.fiarr4ikdev.componentaccountingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@SuppressWarnings("HideUtilityClassConstructor")
+public class ComponentAccountingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComponentAccountingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/fiarr4ikdev/componentaccountingapi/ComponentAccountingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fiarr4ikdev.componentaccountingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponentAccountingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
